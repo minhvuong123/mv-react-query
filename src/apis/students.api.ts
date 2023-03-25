785ed271c857a5e5183feddf9fc66146e3b67bd2
minhvuong123 <minhvuong12222@gmail.com>
2023-03-25 12:49:38 +0700

    react query

---
+++ src/apis/students.api.ts
@@ -0,0 +1,28 @@
+import { Student, Students } from 'types/students.type'
+import http from 'utils/http'
+
+export const getStudents = (page: number | string, limit: number | string, signal?: AbortSignal) => {
+  return http.get<Students>('students', {
+    params: {
+      _page: page,
+      _limit: limit
+    },
+    signal
+  })
+}
+
+export const addStudent = (student: Omit<Student, 'id'>) => {
+  return http.post<Student>('students', student)
+}
+
+export const getStudent = (id: number | string) => {
+  return http.get<Student>(`students/${id}`)
+}
+
+export const updateStudent = (id: number | string, student: Student) => {
+  return http.put<Student>(`students/${id}`, student)
+}
+
+export const deleteStudent = (id: number | string) => {
+  return http.delete<{}>(`students/${id}`)
+}
